29aefb202b72d3f1e7596da3e3e7c381ab700363
chaofu <chaofu@163.com>
2019-07-25 16:45:55 +0800

    处理字段的 枚举和数字,关联类型

--- application/database.php
+++ application/database.php
@@ -18,11 +18,12 @@ return [
     // 服务器地址
     'hostname'        => Env::get('database.hostname', '127.0.0.1'),
     // 数据库名
-    'database'        => Env::get('database.database', 'fitsadminClient'),
+    'database'        => Env::get('database.database', 'fitsadmin'),
     // 用户名
     'username'        => Env::get('database.username', 'root'),
     // 密码
-    'password'        => Env::get('database.password', '123456'),
+    'password'        => Env::get('database.password', 'root
+    '),
     // 端口
     'hostport'        => Env::get('database.hostport', ''),
     // 连接dsn
